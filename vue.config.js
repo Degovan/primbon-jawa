181562e08e2140309144cf0bccc006a1982c35a3
sahrullahh <mohammadsahrullah894@gmail.com>
2022-07-26 18:06:09 +0700

    add: dynamically meta tags, sitemap and setup SEO

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,42 @@
 const { defineConfig } = require("@vue/cli-service");
+const SitemapPlugin = require("sitemap-webpack-plugin").default;
+
+const paths = [
+  {
+    path: "/",
+    lastmod: "2022-7-27",
+    priority: 1.0,
+    changefreq: "yearly",
+  },
+  {
+    path: "/cek-weton",
+    lastmod: "2022-7-27",
+    priority: 0.9,
+    changefreq: "yearly",
+  },
+  {
+    path: "/translate-jawa",
+    lastmod: "2022-7-27",
+    priority: 0.9,
+    changefreq: "yearly",
+  },
+  {
+    path: "/kalender-jawa",
+    lastmod: "2022-7-27",
+    priority: 0.9,
+    changefreq: "yearly",
+  },
+  {
+    path: "/contact",
+    lastmod: "2022-7-27",
+    priority: 0.9,
+    changefreq: "yearly",
+  },
+];
+
 module.exports = defineConfig({
-  // transpileDependencies: true
-  transpileDependencies: ["vue-meta"],
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [new SitemapPlugin({ base: "https://primbonjawa.net", paths })],
+  },
 });
